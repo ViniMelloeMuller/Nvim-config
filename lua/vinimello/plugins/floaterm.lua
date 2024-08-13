45909642c391af88649d1392b1e3250a50a33916
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2024-08-13 20:58:00 -0300

    Updated floaterm to autocmd files

--- lua/vinimello/plugins/floaterm.lua
+++ lua/vinimello/plugins/floaterm.lua
@@ -5,5 +5,11 @@ return {
 			vim.g.floaterm_keymap_new    = '<F7>'
 			vim.g.floaterm_keymap_toggle = '<F12>'
 			vim.g.floaterm_position = 'topright'
+			vim.cmd([[
+			autocmd filetype c nnoremap <F4> :w <bar> :FloatermNew gcc % -o main.exe <CR>
+			autocmd filetype cpp nnoremap <F4> :w <bar> :FloatermNew g++ % -o main.exe <CR>
+			autocmd filetype python nnoremap <F4> :w <bar> :FloatermNew --autoclose=0 python3 % <CR>
+			autocmd filetype fortran nnoremap <F4> :w <bar> :FloatermNew gfortran % -o main.exe <CR>
+			]])
 			end
 }
