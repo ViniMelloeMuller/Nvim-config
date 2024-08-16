bcdd36eda4ab14e35b07fdd9d253ce9fc16eaf53
Vinicius - iS <viniciusmello@i.systems.com.br>
2024-08-16 14:13:01 -0300

    added some keymaps

--- lua/vinimello/plugins/telescope.lua
+++ lua/vinimello/plugins/telescope.lua
@@ -3,4 +3,11 @@ return {
 	tag = '0.1.8',
 	branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	opts = {},
+	init = function ()
+		local builtin = require('telescope.builtin')
+		vim.api.nvim_set_keymap('n', '<leader>g', ":Telescope live_grep <CR>", {noremap=true})
+		vim.api.nvim_set_keymap('n', '<leader>F', ":Telescope find_files <CR>", {noremap=true})
+		vim.api.nvim_set_keymap('n', '<leader>b', ":Telescope buffers <CR>", {noremap=true})
+	end,
 }
