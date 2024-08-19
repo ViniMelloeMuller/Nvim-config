24b399f69a2ef7d0003053c4a21c446148a57b11
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2024-08-19 15:59:38 -0300

    autocompletion working

--- lua/vinimello/lsp-config.lua
+++ lua/vinimello/lsp-config.lua
@@ -23,7 +23,6 @@ end
 lsp_zero.extend_lspconfig({
   sign_text = true,
   lsp_attach = lsp_attach,
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
 require('mason').setup({})
@@ -35,34 +34,3 @@ require('mason-lspconfig').setup({
   }
 })
 
----
--- Autocompletion config
----
-local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
-
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.vim_snippet_jump_forward(),
-    ['<C-b>'] = cmp_action.vim_snippet_jump_backward(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-})
--- lua/vinimello/plugins/lsp.lua
+++ lua/vinimello/plugins/lsp.lua
@@ -3,7 +3,5 @@ return {
 	{'williamboman/mason-lspconfig.nvim'},
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
 	{'L3MON4D3/LuaSnip'},
 }
