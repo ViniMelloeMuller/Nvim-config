96b18d9d76f405e367bed0338b64990911930bed
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2024-08-24 18:55:01 -0300

    added markdown rendering

--- lua/vinimello/plugins/treesitter.lua
+++ lua/vinimello/plugins/treesitter.lua
@@ -2,14 +2,14 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function () 
+		config = function ()
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html"},
 				sync_install = false,
 				highlight = { enable = true },
-				indent = { enable = true },  
+				indent = { enable = true },
 			})
 		end
 	}
