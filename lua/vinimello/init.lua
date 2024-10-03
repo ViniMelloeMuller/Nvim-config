33cef7bd0c76592d07b2adc54a2224a070d4bee8
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2024-10-03 16:44:18 -0300

    Updated the vim defaults

--- lua/vinimello/init.lua
+++ lua/vinimello/init.lua
@@ -2,10 +2,12 @@
 
 vim.cmd([[
 	:set autoindent
+	:set autoread
 	:set clipboard=unnamed
 	:set encoding=UTF-8
 	:set mouse=a
 	:set noundofile
+	:set nobackup
 	:set number
 	:set relativenumber
 	:set shiftwidth=4
@@ -18,6 +20,8 @@ vim.cmd([[
 	nnoremap <SPACE> <NOP>
 	nnoremap <leader>f /
 	map <silent> <leader><cr> :noh<cr>
+	nmap <leader>w :w!<cr>
+	map 0 ^
 ]])
 
 require("vinimello.plugins")
