d30fe879475e7cbe540d2954cad34bf3181a0e07
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2025-02-26 22:09:55 -0300

    ollama funcionando como uma opção

--- lua/vinimello/plugins/codecompanion.lua
+++ lua/vinimello/plugins/codecompanion.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		require("codecompanion").setup({
 			adapters={
+				opts = {show_defaults=false,},
 				deepseek = function ()
 					return require("codecompanion.adapters").extend("deepseek", {
 						env = {
@@ -15,11 +16,19 @@ return {
 						}
 					})
 				end,
+				ollama = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						env = {
+							model = "qwen2.5-coder:3b",
+							url = os.getenv("OLLAMA_HOST") or "http://localhost:11343",
+						}
+					})
+				end,
 			},
 			strategies = {
-				chat   = {adapter = "deepseek", },
-				inline = {adapter = "deepseek", },
-				agent  = {adapter = "deepseek", },
+				chat   = {adapter = "deepseek",},
+				inline = {adapter = "deepseek",},
+				agent  = {adapter = "deepseek",},
 			},
 		})
 	end
