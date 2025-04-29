30d626a8a3d1fea63a1a720fee3b458f66a65fb9
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2025-04-29 00:17:22 -0300

    fix macro not showing error

--- lua/vinimello/plugins/noice.lua
+++ lua/vinimello/plugins/noice.lua
@@ -1,28 +1,47 @@
 -- lazy.nvim
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    presets = {
-      bottom_search=false,
-      command_palette=false,
-      inc_rename=false,
-      lsp_doc_border=false,
-      long_message_to_split=false
-    },
-    notify = {
-      enabled=true,
-    }
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    {
-       "rcarriga/nvim-notify",
-       opts = {
-        render = "compact",
-        stages = "fade",
-        timeout = 2000,
-       },
-    },
-  },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		presets = {
+			bottom_search=false,
+			command_palette=false,
+			inc_rename=false,
+			lsp_doc_border=false,
+			long_message_to_split=false
+		},
+		notify = {
+			enabled=true,
+		}
+	},
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond=require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					}
+				},
+			},
+		})
+
+		require("noice").setup({
+			cmdline = {
+				view="cmdline",
+			}
+		})
+	end,
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				render = "compact",
+				stages = "fade",
+				timeout = 2000,
+			},
+		},
+	},
 }
