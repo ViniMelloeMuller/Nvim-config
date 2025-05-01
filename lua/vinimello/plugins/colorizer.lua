21887ce7f9d02e0ad18fcbfe32b0476dd69ff6ad
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2025-05-01 00:53:18 -0300

    update `colorizer` config

--- lua/vinimello/plugins/colorizer.lua
+++ lua/vinimello/plugins/colorizer.lua
@@ -1,6 +1,9 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	config = function ()
-		require("colorizer").setup()
-	end
+		require("colorizer").setup({
+			'*';
+			css = { rgb_fn = true; };
+		})
+	end,
 }
