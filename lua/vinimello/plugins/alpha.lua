2200e269453e943ddb8141ee53412fbfae9d7122
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2025-05-03 10:10:03 -0300

    change buttons

--- lua/vinimello/plugins/alpha.lua
+++ lua/vinimello/plugins/alpha.lua
@@ -7,8 +7,10 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
 			dashboard.button( "lg","  > LazyGit"  , ":LazyGit<CR>"),
-			dashboard.button( "f", "󰈞  > Find file", ":cd $HOME/Documents/Estudos/ | Telescope find_files<CR>"),
+			dashboard.button( "f", "󰈞  > Find file", ":Telescope find_files<CR>"),
+			dashboard.button( "g", "󰈞  > Ripgrep", ":Telescope live_grep<CR>"),
 			dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+			dashboard.button( "u", "󰚰  > Update Packages"   , ":Lazy update<CR>"),
 			dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 			dashboard.button( "q", "󰈆  > Quit NVIM", ":qa<CR>"),
 		}
