cc8ffcc79c291a228c3b0ccc1adb47f8585b18ab
ViniMelloeMuller <viniciusmelloemuller@gmail.com>
2025-05-03 10:09:50 -0300

    add transparent background to colorscheme

--- lua/vinimello/plugins/colorscheme.lua
+++ lua/vinimello/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
 	'catppuccin/nvim',
 	name="catppuccin",
 	priority = 1000,
+	opts = {
+		transparent_background=true,
+	},
 	init = function()
 		vim.cmd[[colorscheme catppuccin-latte]]
 	end
