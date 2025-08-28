c52e85a83c1ece5df2bbd914e4c81ec0a94d6aea
vinimello-iS <vinicius.mello@i.systems.com.br>
2025-08-28 08:33:46 -0300

    fix deprecation error

--- lua/vinimello/plugins/noice.lua
+++ lua/vinimello/plugins/noice.lua
@@ -19,8 +19,6 @@ return {
 			sections = {
 				lualine_x = {
 					{
-						require("noice").api.statusline.mode.get,
-						cond=require("noice").api.statusline.mode.has,
 						color = { fg = "#ff9e64" },
 					}
 				},
