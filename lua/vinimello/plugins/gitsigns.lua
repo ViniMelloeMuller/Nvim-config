0d5c3396131db94aa41f29344415211a61e08d18
Vinicius Mello e Muller <viniciusmelloemuller@gmail.com>
2025-12-31 15:50:20 -0300

    add git jump to next diff

--- lua/vinimello/plugins/gitsigns.lua
+++ lua/vinimello/plugins/gitsigns.lua
@@ -1,4 +1,22 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {},
+	config = function ()
+		require("gitsigns").setup()
+		local gs = require('gitsigns')
+		
+		-- Pular para a PRÓXIMA mudança (next change)
+		vim.keymap.set('n', ']]', function()
+		  if vim.wo.diff then return ']]' end
+		  vim.schedule(function() gs.nav_hunk('next') end)
+		  return '<Ignore>'
+		end, { expr = true, desc = "Próxima alteração Git" })
+
+		-- Pular para a mudança ANTERIOR (prev change)
+		vim.keymap.set('n', '[[', function()
+		  if vim.wo.diff then return '[[' end
+		  vim.schedule(function() gs.nav_hunk('prev') end)
+		  return '<Ignore>'
+		end, { expr = true, desc = "Alteração Git anterior" })
+	end
 }
