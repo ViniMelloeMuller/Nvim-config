6cb97f9d57c7b702ecbb339d84c425202c2b6230
Vinicius Mello e Muller <viniciusmelloemuller@gmail.com>
2026-01-02 17:55:03 -0300

    add persistence

--- lua/vinimello/plugins/alpha.lua
+++ lua/vinimello/plugins/alpha.lua
@@ -3,9 +3,13 @@ return {
     config = function ()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+		local session_button = dashboard.button(
+			"c", " > Restore Session", ':lua require("persistence").load()<CR>'
+		)
 
 		dashboard.section.buttons.val = {
-			dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+			-- dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+			session_button,
 			dashboard.button( "lg","  > LazyGit"  , ":LazyGit<CR>"),
 			dashboard.button( "f", "󰈞  > Find file", ":Telescope find_files<CR>"),
 			dashboard.button( "g", "󰈞  > Ripgrep", ":Telescope live_grep<CR>"),
