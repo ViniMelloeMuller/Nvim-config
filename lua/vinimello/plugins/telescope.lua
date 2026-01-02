af400b4f4b08bcfb6afb4047126a05913b2f2fe9
Vinicius Mello e Muller <viniciusmelloemuller@gmail.com>
2026-01-02 15:50:36 -0300

    fix telescope

--- lua/vinimello/plugins/telescope.lua
+++ lua/vinimello/plugins/telescope.lua
@@ -1,8 +1,6 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build='make' } },
 	opts = {},
 	init = function ()
 		local builtin = require('telescope.builtin')
