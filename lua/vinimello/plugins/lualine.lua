7af660567ccbcb197f72f4f68e8ef757d3f23d01
Vinicius Mello e Muller <viniciusmelloemuller@gmail.com>
2026-01-02 15:57:27 -0300

    update lualine

--- lua/vinimello/plugins/lualine.lua
+++ lua/vinimello/plugins/lualine.lua
@@ -1,29 +1,83 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			-- hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
 	opts = function()
-		return {
-			theme = "dracula",
+		local lualine_require = require("lualine_require")
+		lualine_require.require = require
+		vim.o.laststatus = vim.g.lualine_laststatus
+
+		local opts = {
+			options = {
+				theme = "auto",
+				globalstatus = vim.o.laststatus == 3,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+
 				lualine_x = {
+					Snacks.profiler.status(),
+					-- stylua: ignore
+					{
+						function() return require("noice").api.status.command.get() end,
+						cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+						color = function() return { fg = Snacks.util.color("Statement") } end,
+					},
+					-- stylua: ignore
+					{
+						function() return require("noice").api.status.mode.get() end,
+						cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+						color = function() return { fg = Snacks.util.color("Constant") } end,
+					},
+					-- stylua: ignore
+					{
+						function() return "  " .. require("dap").status() end,
+						cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+						color = function() return { fg = Snacks.util.color("Debug") } end,
+					},
+					-- stylua: ignore
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = function() return { fg = Snacks.util.color("Special") } end,
+					},
+					{
+						"diff",
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_y = {
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_z = {
 					function()
-						local ok, pomo = pcall(require, "pomo")
-						if not ok then
-							return ""
-						end
-
-						local timer = pomo.get_first_to_finish()
-						if timer == nil then
-							return ""
-						end
-
-						return "󰄉 " .. tostring(timer)
+						return " " .. os.date("%R")
 					end,
-					"encoding",
-					"fileformat",
-					"filetype",
 				},
 			},
+			extensions = { "neo-tree", "lazy", "fzf" },
 		}
+		return opts
 	end,
 }
