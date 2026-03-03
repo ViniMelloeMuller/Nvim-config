be98748550e79cf26f239bb40b9e77137cf062ff
vinimello-iS <vinicius.mello@i.systems.com.br>
2026-03-03 18:35:34 -0300

    bugfix: fix colorizer config (#9)
    Co-authored-by: Vinicius Mello <vinicius.mello@isystems.ai>

--- lua/vinimello/plugins/colorizer.lua
+++ lua/vinimello/plugins/colorizer.lua
@@ -1,6 +1,20 @@
 return {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {
+		options = {
+			-- Configurações de parsing (o que o plugin vai ler)
+			parsers = {
+				css = true,
+				hex = true
+			},
+			-- Configurações de exibição (como a cor vai aparecer)
+			display = {
+				mode = "virtualtext", -- Pode ser "background", "foreground" ou "virtualtext"
+				virtualtext = {
+					position = "after" -- Exibe o bloco de cor ao lado do texto
+				},
+			},
+		},
+	},
 }
